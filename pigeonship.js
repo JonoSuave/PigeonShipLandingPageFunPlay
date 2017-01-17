96eece87a50fab577ce27a75ce5c62298f321fa6
Jonathan Duncan <jonosuave@Jonathans-MacBook-Pro.local>
2017-01-17 11:44:11 -0700

    Just finished working on body background and worked on text and See Savings button in first row

--- pigeonship.js
+++ pigeonship.js
@@ -1,9 +1,10 @@
+
 $(document).ready(function() {
-   $('.section-description').on('mouseenter', function(event){
-       event.preventDefault();
-      $(this).closest('.section-description').find('.description').slideToggle(); 
-   }); 
-    $('.section-description').on('mouseleave'), function(event){
-        $(this).closest('.section-desctription').find('.description').slideDown();
-    }
+    $('.section-description').on('click', function(event){
+        event.preventDefault();
+           $(this).closest('.section-description').find('.description').slideToggle(); 
+    }); 
+    $('.section-description').on('mouseleave', function(event){
+        $(this).find(".description").slideUp(); 
+    });
 });
