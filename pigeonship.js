cfca663d4b7a94aecef497824f392555110065ff
Jonathan Duncan <jonosuave@Jonathans-MacBook-Pro.local>
2017-01-17 08:33:20 -0700

    slideToggle for all three bottom columns, but having trouble with it disappearing after mouseleave

--- pigeonship.js
+++ pigeonship.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-   $('.section-description').on('click', function(event){
+   $('.section-description').on('mouseenter', function(event){
        event.preventDefault();
       $(this).closest('.section-description').find('.description').slideToggle(); 
    }); 
+    $('.section-description').on('mouseleave'), function(event){
+        $(this).closest('.section-desctription').find('.description').slideDown();
+    }
 });
